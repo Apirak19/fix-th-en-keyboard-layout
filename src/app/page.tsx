9dd46cbd8d02baaef3b7e0b549039f81fff350d7
Apirak <apirakfakin@gmail.com>
2023-11-27 15:41:28 +0700

    editeText in textarea

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,26 +6,34 @@ export default function Home() {
   const [inputText, setInputText] = useState("");
   const [editedText, setEditedText] = useState("");
 
-  function handleInput(event: React.ChangeEvent<HTMLInputElement>): void {
+  function handleInput(event: React.ChangeEvent<HTMLTextAreaElement>): void {
     const newText = EngToThai(event.target.value);
     setInputText(newText);
+    setEditedText(inputText);
   }
   function EngToThai(text: string): string {
     return "fixed" + text;
   }
-  function handleButton(): void {
-    setEditedText(inputText);
-  }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Misspelled Texts Fix</h1>
-      <input type="text" onChange={handleInput} />
-      <button type="button" onClick={handleButton}>
-        Edit
-      </button>
-      <span className="font-bold">Your text in English</span>
-      <p>{editedText}</p>
+    <main className="min-h-screen p-24">
+      <h1 className="text-center font-bold text-5xl">Misspelled Texts Fix</h1>
+      <div className="flex flex-col items-center my-5">
+        <span className="font-bold mt-7">Put your text in Thai</span>
+        <textarea
+          className="w-[700px] my-3"
+          style={{ borderRadius: "10px" }}
+          rows={4}
+          onChange={handleInput}
+        />
+        <span className="font-bold mt-7">Your text in English</span>
+        <textarea
+          className="w-[700px] my-3"
+          style={{ borderRadius: "10px" }}
+          rows={4}
+          value={editedText}
+        />
+      </div>
     </main>
   );
 }
